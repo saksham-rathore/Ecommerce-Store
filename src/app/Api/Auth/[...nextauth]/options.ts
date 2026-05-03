f202defec4cbf008a5090e6736e3de6642bd8e65
saksham762007 <samrathore07062007@gmail.com>
2026-05-04 00:04:50 +0530

    feat: initialize NextAuth configuration with credentials provider and database connection setup

---
+++ src/app/Api/Auth/[...nextauth]/options.ts
@@ -0,0 +1,28 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbconnect";
+import UserModel from "@/Model/User";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+        try {
+            const user = await UserModel.findOne({
+                
+            })
+        } catch (err: any) {
+          throw new Error(err);
+        }
+      },
+    }),
+  ],
+};
