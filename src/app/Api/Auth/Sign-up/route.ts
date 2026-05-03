2cf08206e0bd19eba0da935dfa3d3cec4065b7b6
saksham762007 <samrathore07062007@gmail.com>
2026-05-03 22:02:18 +0530

    feat: implement user registration POST route handler with database connection and password hashing setup

--- src/app/Api/Auth/Sign-up/route.ts
+++ src/app/Api/Auth/Sign-up/route.ts
@@ -2,3 +2,22 @@ import dbConnect from "@/lib/dbconnect";
 import UserModel from "@/Model/User";
 import bcrypt from "bcryptjs";
 
+
+export async function POST(request: Request){
+    await dbConnect()
+
+    try {
+        const {username, email, password} = await request.json()
+    } catch (error) {
+        console.error("Error registering user", error)
+        return Response.json(
+            {
+                success: false,
+                message: "Error registering User"
+            },
+            {
+                status: 500
+            }
+        )
+    }
+} 
