88243d58c2c74371c81b329bef2b41eb7c3f55ef
saksham762007 <samrathore07062007@gmail.com>
2026-05-04 15:07:28 +0530

    feat: implement NextAuth credentials provider with MongoDB database authentication

--- src/app/Api/Auth/[...nextauth]/options.ts
+++ src/app/Api/Auth/[...nextauth]/options.ts
@@ -3,6 +3,8 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import dbConnect from "@/lib/dbconnect";
 import UserModel from "@/Model/User";
+import { Session } from "next-auth";
+import { JWT } from "next-auth/jwt";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -16,34 +18,44 @@ export const authOptions: NextAuthOptions = {
       async authorize(credentials: any): Promise<any> {
         await dbConnect();
         try {
-            const user = await UserModel.findOne({
-                $or: [
-                    {email: credentials.identifier},
-                    {username: credentials.identifier}
-                ]
-            })
-            if (!user) {
-                throw new Error("No user found with this email")
-            }
-            const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password)
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier },
+              { username: credentials.identifier },
+            ],
+          });
+          if (!user) {
+            throw new Error("No user found with this email");
+          }
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
 
-            if (isPasswordCorrect) {
-                return user
-            } else {
-                throw new Error("Incorrect Password!")
-            }
+          if (isPasswordCorrect) {
+            return user;
+          } else {
+            throw new Error("Incorrect Password!");
+          }
         } catch (err: any) {
           throw new Error(err);
         }
       },
     }),
   ],
+  callbacks: {
+    async session({ session, token }: { session: Session; token: JWT }) {
+      return session;
+    },
+    async jwt({ token, user }) {
+      return token;
+    },
+  },
   pages: {
-    signIn: '/sign-in'
+    signIn: "/sign-in",
   },
   session: {
-    strategy: "jwt"
+    strategy: "jwt",
   },
   secret: process.env.NEXT_AUTH,
-  
 };
